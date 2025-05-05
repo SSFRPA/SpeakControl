eb10118995a2b736cbc51189d5c9055c381d4f89
lugof <ybbtuubj@163.com>
2025-05-06 02:58:08 +0800

    使用nircmd模拟win+d 修复显示桌面问题

--- modules/日常命令/显示桌面.js
+++ modules/日常命令/显示桌面.js
@@ -1,4 +1,10 @@
 
 export function run(text) {
-    ssf.Windows.run("explorer.exe", ["shell:::{3080F90D-D7AD-11D9-BD98-0000947B0257}"]);
+    // ssf.Windows.run("explorer.exe", ["shell:::{3080F90D-D7AD-11D9-BD98-0000947B0257}"]);
+    // ssf.Windows.run("nircmd.exe", ["win", "minimizeall"]);
+
+    ssf.Windows.cmd("./ext_tools/nircmd-x64/nircmd.exe", [
+        "sendkeypress",
+        "rwin+d",
+    ]);
 }
